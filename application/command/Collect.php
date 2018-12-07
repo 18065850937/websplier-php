316191875a1f955cf6a37d9af6e435a70215dd51
liaosp <runsp@gitee.com>
2018-12-07 20:11:28 +0800

    修改了一些问题

--- application/command/Collect.php
+++ application/command/Collect.php
@@ -10,6 +10,7 @@
  */
 
 namespace app\command;
+use app\c\template\Cnblogs;
 use think\console\Command;
 use think\console\Input;
 use think\console\Output;
@@ -24,6 +25,7 @@ class Collect extends Command
 
     protected function execute(Input $input, Output $output)
     {
+        (new Cnblogs())->getIndexWebSite();
         $output->writeln("正在执行");
     }
 
